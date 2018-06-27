cd7ad16bee6e6597dbc654f4f2bd9b2f12751a78
RitaJ <ritaj777@gmail.com>
2018-06-27 10:31:14 +0300

    create migrations files, models and controllers for item and category

---
+++ database/migrations/2018_06_27_065660_create_items_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('quantity');
+            $table->float('price');
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        Schema::table('items', function (Blueprint $table) {
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
